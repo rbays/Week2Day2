b4dd619f7250dfffca0612e11514b0c340359bfd
Robert Bays <bays.robert@gmail.com>
2016-06-21 16:11:30 +0100

    started library

---
+++ Library/Library/Book.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library
+{
+    class Book
+    {
+        private string _title;
+        private string author;
+        private string blurb;
+        private string genre;
+        private bool reference;
+        private bool inStock;
+
+        public Book(string title, string author, string blurb, string genre, bool reference)
+        {
+            this._title = title;
+            this.author = author;
+            this.blurb = blurb;
+            this.genre = genre;
+            this.reference = reference;
+            this.inStock = true;
+        }
+        
+        public string title { get
+            {
+                return _title;
+            } }
+
+        public void borrowBook()
+        {   
+            if (reference)
+            {
+                Console.WriteLine("Book is a reference copy only.");
+            }
+            else if (this.inStock)
+            {
+                Console.WriteLine("Enjoy your book.");
+                this.inStock = false;
+            }
+            else
+            {
+                Console.WriteLine("Book is currently out.");
+            }
+        }
+        public void returnBook()
+        {
+            if (this.inStock)
+            {
+                Console.WriteLine("Book is already in stock, you can't return it");
+            }
+            else
+            {
+                Console.WriteLine("Thank you, I hope it was a good read");
+                this.inStock = true;
+            }
+        }
+        public void getInfo()
+        {
+            Console.WriteLine("Title: {0}", this._title);
+            Console.WriteLine("Author: {0}", this.author);
+            Console.WriteLine("Genre: {0}", this.genre);
+            Console.WriteLine("Blurb: {0}", this.blurb);
+            if (reference)
+            {
+                Console.WriteLine("This is a reference copy book");
+            }
+        }
+        
+    }
+}
---
+++ Library/Library/Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Book> bookList = new List<Book> { };
+            Console.WriteLine("Welcome to the Library, what would you like to do?");
+            bool running = true;
+            while (running)
+            {
+                Console.WriteLine("OPTIONS: donate list remove borrow return check exit");
+                string choice = Console.ReadLine().ToLower();
+                switch (choice)
+                {
+                    case "donate":
+                        Console.WriteLine("What is the title?");
+                        string titleIn = Console.ReadLine();
+                        Console.WriteLine("Who is the Author?");
+                        string authorIn = Console.ReadLine();
+                        Console.WriteLine("What is the genre?");
+                        string genreIn = Console.ReadLine();
+                        Console.WriteLine("Write a quick synopsis");
+                        string blurbIn = Console.ReadLine();
+                        string referenceIn = "";
+                        bool refbool;
+                        while (!(referenceIn == "y" || referenceIn == "n"))
+                        {
+                            Console.WriteLine("is this a reference copy? (Y/N)");
+                            referenceIn = Console.ReadLine().ToLower();
+                        }
+                        switch (referenceIn)
+                        {
+                            case "y":
+                                refbool = true;
+                                break;
+                            case "n":
+                                refbool = false;
+                                break;
+                            default:
+                                refbool = false;
+                                break;
+                        }
+                        bookList.Add(new Book(titleIn, authorIn, blurbIn, genreIn, refbool));
+                        break;
+                    case "List":
+                        Console.WriteLine("Titles: ");
+                        foreach(Book b in bookList)
+                        {
+                            Console.WriteLine(b.title);
+                        }
+                        break;
+                    case "remove":
+                        Console.WriteLine("What book would you like to permenantly remove from the library?");
+                        string removeIn = Console.ReadLine();
+                        int copies = 0;
+                        foreach (Book b in bookList)
+                        {
+                            if(b.title == removeIn)
+                            {
+                                bookList.Remove(b);
+                                Console.WriteLine("book removed");
+                                copies += 1;
+                            }
+                        }
+                        if (copies == 0)
+                        {
+                            Console.WriteLine("Book not in library");
+                        }
+                        break;
+                    case "borrow":
+
+
+                }
+            }
+
+        }
+    }
+}
